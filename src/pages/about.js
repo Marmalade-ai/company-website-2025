d77b0f33f9bb2429e6d7bc9b30b1fa4bbd274d55
craigfisk <craigfisk@users.noreply.github.com>
2023-02-19 22:17:29 -0600

    Added "gatsby-source-filesystem" to gatsby-config.js and changed globally to node 14.17.6 and npm 7.24.0.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import Seo from '../components/seo'
 
 const AboutPage = () => {
   return (
     <Layout pageTitle='About Me'>
-      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+      <p>Hi there! We're the proud operators of this site, built with Gatsby 5.x. Original development work by Justin Blake and Jim Tyhurst. Updated in 2023 by Craig Fisk.</p>
     </Layout>
   )
 }
 
-export const Head = () => <title>About Me</title>
+export const Head = () => <Seo title="About Me" />
 
 export default AboutPage
