6780a37e15ab01fa69ced1ffa984f20a25992cd9
craigfisk <craigfisk@users.noreply.github.com>
2023-02-25 10:41:43 -0600

    Added founder bios and photos to About page.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,106 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import Seo from '../components/seo'
+import { StaticImage } from 'gatsby-plugin-image'
+import Card from '../components/Card';
+
 
 const AboutPage = () => {
   return (
     <Layout pageTitle='About Me'>
-      <p>Hi there! We're the proud operators of this site, built with Gatsby 5.x. Original development work by Justin Blake and Jim Tyhurst. Updated in 2023 by Craig Fisk.</p>
+      <p>Hi there! We're the proud operators of this site, built with Gatsby 5.x, Tailwind, and Google Analytics 4. Original development work by Justin Blake and Jim Tyhurst. Updated in 2023 by Craig Fisk.</p>
+    
+
+      <section id="founders" className="pt-4">
+      <div className="container mx-auto">
+        <h2>Founders</h2>
+        <div className="container">
+          <p>
+            We live in interesting times and innovation is key to the future.
+          </p>
+          <p>
+            It&apos;s great to be developing something where the people using it
+            and the people developing it can be one and the same. We came
+            together through user research into the problems people have with
+            networking and each of us decided it was possible to contribute to
+            making it better.
+          </p>
+        </div>
+        <div className="pt-8 grid grid-cols-1 flex-col -mx-3 md:grid-cols-2">
+          <div className="flex-1 px-3">
+            <Card className="mb-8">
+              <StaticImage
+                className="mb-8 w-32 h-32 rounded-lg shadow-lg"
+                src="../images/craig_fisk_headshot.jpg"
+                alt="Photo of Craig Fisk."
+                title="Craig Fisk, CEO"
+              />
+              <h3 className="mt-0">Craig Fisk</h3>
+              <p className="cardSubHeading">CEO</p>
+              <p>
+                Series A mobile CRM, new software products/channels at Intel.
+              </p>
+              <p>Chicago Booth MBA.</p>
+              <p>San Jose, CA.</p>
+            </Card>
+          </div>
+          <div className="flex-1 px-3">
+            <Card className="mb-8">
+              <StaticImage
+                className="mb-8 w-32 h-32 rounded-lg shadow-lg	"
+                src="../images/jim_tyhurst_headshot.jpg"
+                alt="Photo of Jim Tyhurst."
+                title="Jim Tyhurst, CTO"
+              />
+              <h3 className="mt-0">Jim Tyhurst</h3>
+              <p className="cardSubHeading">CTO</p>
+              <p>
+                Software architect, Machine Learning / Natural Language
+                Processing (ML/NLP), independent consultant.
+              </p>
+              <p>UCLA PhD.</p>
+              <p>Portland, OR.</p>
+            </Card>
+          </div>
+          <div className="flex-1 px-3">
+            <Card className="mb-8">
+              <StaticImage
+                className="mb-8 w-32 h-32 rounded-lg shadow-lg	"
+                src="../images/vivian_tran_headshot.jpg"
+                alt="Photo of Vivian Tran."
+                title="Vivian Tran, Marketing Advisor"
+              />
+              <h3 className="mt-0">Vivian Tran</h3>
+              <p className="cardSubHeading">Marketing Advisor</p>
+              <p>
+                New segment marketing, healthcare industry marketing manager.
+              </p>
+              <p>UC Berkeley.</p>
+              <p>Berkeley, CA.</p>
+            </Card>
+          </div>
+          <div className="flex-1 px-3">
+            <Card className="mb-8">
+              <StaticImage
+                className="mb-8 w-32 h-32 rounded-lg shadow-lg	"
+                src="../images/michael_porto_headshot.jpg"
+                alt="Photo of Michael Porto."
+                title="Michael Porto, Product Management Advisor"
+              />
+              <h3 className="mt-0">Michael Porto</h3>
+              <p className="cardSubHeading">Product Management Advisor</p>
+              <p>Big data, mobile, ML, Intel, Wells Fargo, EY.</p>
+              <p>Princeton MS CS.</p>
+              <p>Charlotte, NC.</p>
+            </Card>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+
+    
     </Layout>
   )
 }
