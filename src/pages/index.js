0495b4d1c5a2beaeaa52b962b0a70dd754a9c278
craigfisk <craigfisk@users.noreply.github.com>
2023-02-24 23:28:22 -0600

    In index.js, added a button with Tailwind styling for the Calendly link.

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,14 +7,15 @@ import { Link } from 'gatsby'
 const IndexPage = () => {
   return (
     <Layout pageTitle="">
-      <p style={{fontFamily: "Sans-serif"}}>Marmalade AI is <u>virtual networking</u> for software people in <b>UX design</b>, 
-        <b>digital marketing</b>, or <b>machine learning</b> who really need to connect with each other and are 
+      <p style={{fontFamily: "Sans-serif"}}>Marmalade AI is an app that provides <u>virtual networking</u> for software devs in <b>UX design</b>, 
+         <b>digital marketing</b>, or <b>machine learning</b> who really need to connect with each other and are 
         dissatisfied with traditional networking. Those are the areas where we're starting. Based on profiles, Marmalade 
         matches specialists with specialists using NLP and machine learning. When, where, and with whom you connect is up to you.
       </p>
       <Link href="https://assets.calendly.com/assets/external/widget.css" rel="stylesheet">
         <script src="https://assets.calendly.com/assets/external/widget.js" type="text/javascript" async></script>
-        <a href="https://calendly.com/wcraigfisk/call-with-craig-fisk-30-minutes" onclick="Calendly.initPopupWidget({url: 'https://calendly.com/wcraigfisk/call-with-craig-fisk-30-minutes'});return false;">Schedule time with me</a>
+        <a href="https://calendly.com/wcraigfisk/call-with-craig-fisk-30-minutes" onclick="Calendly.initPopupWidget({url: 'https://calendly.com/wcraigfisk/call-with-craig-fisk-30-minutes'});return false;">
+          <button class="bg-green-500 text-white px-5 py-2 mt-4 mb-4 rounded">Schedule a demo over Zoom</button></a>
       </Link>
       <StaticImage
         src="../images/emily_screen_640.png" 
