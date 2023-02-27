e776771cc318576862170daa06d84cd4d71d627d
craigfisk <craigfisk@users.noreply.github.com>
2023-02-27 17:05:56 -0600

    Added simple <button> linked to Calendly event in index.js.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import * as React from 'react'
 import Layout from '../components/layout'
 import { StaticImage } from 'gatsby-plugin-image'
 import Seo from '../components/seo'
-import { Link } from 'gatsby'
+import { navigate } from 'gatsby'
 
 const IndexPage = () => {
   return (
@@ -12,12 +12,13 @@ const IndexPage = () => {
         dissatisfied with traditional networking. Those are the areas where we're starting. Based on profiles, Marmalade 
         matches specialists with specialists using NLP and machine learning. When, where, and with whom you connect is up to you.
       </p>
-      <Link to="https://assets.calendly.com/assets/external/widget.css" rel="stylesheet">
+      {/* <Link to="https://assets.calendly.com/assets/external/widget.css" rel="stylesheet">
         <script src="https://assets.calendly.com/assets/external/widget.js" type="text/javascript" async></script>
         <a href="https://calendly.com/wcraigfisk/call-with-craig-fisk-30-minutes" onClick="Calendly.initPopupWidget({url: 'https://calendly.com/wcraigfisk/call-with-craig-fisk-30-minutes'});return false;">
           {/* <button class="bg-green-500 text-white px-5 py-2 mt-4 mb-4 rounded">Schedule a demo over Zoom</button></a> */}
-          <button className="bg-red-500 text-white px-4 py-2 mt-3 mb-4 rounded">Schedule a demo over Zoom</button></a>
-      </Link>
+          {/* <button className="bg-red-500 text-white px-4 py-2 mt-3 mb-4 rounded">Schedule a demo over Zoom</button></a> */}
+      {/* </Link> */}
+      <button onClick={()=>{navigate("https://calendly.com/wcraigfisk/call-with-craig-fisk-30-minutes")}}>Schedule a Demo over Zoom</button>
       <StaticImage
         src="../images/emily_screen_640.png" 
         alt="Emily screen from Marmalade AI" 
