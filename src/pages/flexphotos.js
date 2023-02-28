79a1b0b3698da9d5a63bef90193cff404f70e4e4
craigfisk <craigfisk@users.noreply.github.com>
2023-02-27 21:52:44 -0600

    Added a flexphotos.js page to experiment with Tailwind CSS, and installed Tailwind CSS, per https://tailwindcss.com/docs/guides/gatsby. Remember to put --legacy-peer-deps on the tail of any npm install commands.

--- src/pages/flexphotos.js
+++ src/pages/flexphotos.js
@@ -4,12 +4,15 @@ import Seo from '../components/seo';
 
 const PrivacyPolicy = () => (
   <Layout>
+    <h1 className="text-3xl font-bold underline">
+        Hello world!
+    </h1>
     <Seo title="Privacy Policy" description="Privacy Policy" />
     <div class="flex">
-      <div>block 1</div>
-      <div>block 2</div>
-      <div>block 3</div>
-      <div>block 4</div>
+      <div class="p2 bg-yellow-100">block 1</div>
+      <div class="p2 bg-blue-100">block 2</div>
+      <div class="p2 bg-green-200">block 3</div>
+      <div class="p2 bg-red-300">block 4</div>
     </div>
   </Layout>
 );
