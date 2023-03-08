1f6925d27ca49fcaaa18ee589c9d19cd2d5d7639
craigfisk <craigfisk@users.noreply.github.com>
2023-03-07 20:11:08 -0600

    Changed logo to Marmalade_Logo_trans_white_M_100.svg because the png does not show up for some reason on phone.

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => (
   <header>
     <nav>
       <h1><StaticImage
-        src="../images/Marmalade_Logo_trans_white_M_100.png"
+        src="../images/Marmalade_Logo_trans_white_M_100.svg"
         alt="The Marmalade AI Logo."
         title="Marmalade AI, Inc."
         width={40}
