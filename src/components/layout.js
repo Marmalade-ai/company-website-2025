3a4f7040ec472e3262b9a0ae6c0f431fedbb25f5
craigfisk <craigfisk@users.noreply.github.com>
2023-03-16 23:24:27 -0500

    In layout.js, removed 2nd h1 tag.

--- src/components/layout.js
+++ src/components/layout.js
@@ -51,7 +51,7 @@ const Layout = ({ pageTitle, children }) => {
       </nav>
       
       <main className="baseText">
-        <h1 className={heading}>{pageTitle}</h1>
+        <div className={heading}>{pageTitle}</div>
         {children}
       </main>
       {/* <div>Testing</div> */}
