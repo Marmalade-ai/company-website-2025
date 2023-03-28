7cb423f21e7a8d1e11dd83b0e906eb564d3d8e2f
craigfisk <craigfisk@users.noreply.github.com>
2023-03-27 21:55:46 -0500

    In tailwind.config.js, needed to define all the screen sizes, including "xs".

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,20 @@ module.exports = {
     "./src/components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    screens: {
+      // from https://tailwindcss.com/docs/screens
+      'xs': "540px", 
+      // ... default Theme screens
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {},
   },
+  variants: {
+    extend: [],
+  },
   plugins: [],
-  screens: {
-    // from https://tailwindcss.com/docs/screens
-    'sm': '640px',
-    'md': '768px',
-    'lg': '1024px',
-    'xl': '1280px',
-    '2xl': '1536px',
-  }
 }
