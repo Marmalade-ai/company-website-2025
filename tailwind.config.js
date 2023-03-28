fa713501ff48dc9a2a0c5f51473aceedf4c4d3e8
craigfisk <craigfisk@users.noreply.github.com>
2023-03-27 21:19:35 -0500

    Set default tailwind.config.js per docs.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/pages/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
+  screens: {
+    // from https://tailwindcss.com/docs/screens
+    'sm': '640px',
+    'md': '768px',
+    'lg': '1024px',
+    'xl': '1280px',
+    '2xl': '1536px',
+  }
 }
