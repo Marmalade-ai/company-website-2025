bea7dc0984c5bee6792ac59b3277972517b6cdc0
craigfisk <craigfisk@users.noreply.github.com>
2023-04-16 00:46:08 -0500

    Text edits in "user research" page, which is now added to the menu at the bottom of index page.

--- src/pages/userresearch.js
+++ src/pages/userresearch.js
@@ -26,29 +26,31 @@ const UserResearch = () => {
 
         <div className="grid sm:grid-cols-2 gap-4">
           <StaticImage height={200} alt="User Research" src="../../images/ux-indonesia-w00FkE6e8zE-unsplash_800.png"/>
-          <p className="largeText gap-5"><b>User Research</b> on networking ("professional networking")<br /><br /><b><span className="purpleFont">Please help!!</span></b></p>
-          <p>Are you a specialist in:</p>
+          <div>
+            <p className="largeText gap-5"><b>User Research</b> on &quot;networking&quot;</p><p>(Meaning: "professional networking")<br /><br /><b><span className="purpleFont">Please help!!</span></b></p>
+            <p>If you are in:</p>
+          </div>
         </div>
         <div className="grid sm:grid-cols-2 gap-4 p-4">
-          <p className="sm:text-lg"><b><input type="checkbox"/> UX Design?</b></p>
+          <p className="p-2 sm:text-lg p-12"><b><input type="checkbox"/> UX Design?</b></p>
           <StaticImage height={200} alt="UX design" src="../../images/alvaro-reyes-KxVlKiqQObU-unsplash_800.png"/>
         </div>
 
         <div className="grid sm:grid-cols-2 gap-4 p-4">
-          <p className="sm:text-lg"><b><input type="checkbox"/> Digital Marketing?</b></p>
+          <p className="p-2 sm:text-lg p-12"><b><input type="checkbox"/> Digital Marketing?</b></p>
           <StaticImage height={200} alt="digital marketing" src="../../images/jason-goodman-Oalh2MojUuk-unsplash_800.png"/>
         </div>
 
         <div className="grid sm:grid-cols-2 gap-4 p-4">
-          <p className="sm:text-lg"><b><input type="checkbox"/> Machine Learning?</b></p>
+          <p className="p-2 sm:text-lg p-12"><b><input type="checkbox"/> Machine Learning?</b></p>
           <StaticImage height={200} alt="machine learing" src="../../images/andy-kelly-0E_vhMVqL9g-unsplash_800.png"/>
         </div>
 
         <div className="grid sm:grid-cols-2 gap-4 p-4">
 
           <div>
-            <p className="largeText"><b>Please click to schedule!</b><br/>
-            if &quot;no,&quot; please forward to specialists you know.</p>
+            <p className="largeText"><b>Please click to schedule!</b></p>
+            <p>If &quot;no,&quot; please forward.</p>
           </div>
 
           <div className="container flex flex-col items-center">
@@ -63,12 +65,12 @@ const UserResearch = () => {
          */}
         <div className="grid sm:grid-cols-2 gap-4">
           <StaticImage width={200} height={200} alt="Free subscription" src="../../images/erik-mclean-cYQtW1HTpbg-unsplash_800.png"/>
-          <p className="py-4"><b>Free Subscription</b>: We appreciate your help.</p>
+          <p className="py-2 sm:py-10"><b>Free Subscription</b>: We appreciate your help.</p>
         </div>
         
         <div className="grid sm:grid-cols-2 gap-4">
           <StaticImage width={200} height={200} alt="Input into the design" src="../../images/med-badr-chemmaoui-ZSPBhokqDMc-unsplash_800.png"/>
-          <p className="py-4"><b>Make it work for you!</b> Here's your chance.</p>
+          <p className="py-2 sm:py-10"><b>Make it work for you!</b> Here's your chance.</p>
         </div>
 
         <div>
@@ -101,26 +103,26 @@ const UserResearch = () => {
         </div>
 
         <div className="largeText p-10">
-          <b>Ok, that's a start. But what do &quot;accurate&quot; and &quot;automatic&quot; mean to you? Also, here are further questions</b>:
+          <b>Ok, that's a start. But what do &quot;accurate&quot; and &quot;automatic&quot; mean to you? Discuss! Also, here are further questions</b>:
         </div>
 
         <div className="grid sm:grid-cols-2 gap-4">
           <StaticImage alt="What geographical scope?" src="../../images/pexels-photomix-company-5921677_800.png"/>
-          <p className="py-4"><b>What geographical scope?</b> Should your scope be a city, a metro area, a state, a region, like &quot;Upper Midwest&quot; or &quot;West Coast&quot;? What is a geography UI?</p>
+          <p className="py-4"><b>What&apos;s the geographical scope?</b> Should it be a city, a metropolitan area, a state, a region (like &quot;Upper Midwest&quot; or &quot;West Coast&quot;)? What is the UI?</p>
         </div>
         
         <div className="grid sm:grid-cols-2 gap-4">
           <StaticImage alt="How to contact people?" src="../../images/pexels-alteredsnaps-12699062_800.png"/>
-          <p className="py-4"><b>How to contact people?</b> From video calls to calendar scheduling. or controlling whether you can be contacted at all, or about what -- more questions.</p>
+          <p className="py-4"><b>How to contact people?</b> Video calls? Calendar scheduling? What about whether you can be contacted at all, or about what? -- more questions!</p>
         </div>
         
         <div className="grid sm:grid-cols-2 gap-4">
-          <StaticImage alt="How to work with your history?" src="../../images/pexels-mikhail-nilov-7887040_800.png"/>
-          <p className="py-4"><b>How to work with your history?</b><br />Your network is like affective reality; it changes when you change your profile. What if you want to connect with someone from your &quot;old network&quot;?</p>
+          <StaticImage alt="What about your &quot;history&quot;?" src="../../images/pexels-mikhail-nilov-7887040_800.png"/>
+          <p className="py-4"><b>What about your &quot;history&quot;?</b> &quot;Affective reality&quot;? If your network changes when you change your profile? How to contact someone from your &quot;old network&quot;?</p>
         </div>
 
         <div className="largeText p-10">
-          <b>Piqued your curiosity? Please book a time to talk with us!</b>:
+          <b>Piqued your curiosity? Please book a time to talk with us!</b>
         </div>
 
         <div className="container flex flex-col items-center">
@@ -128,7 +130,7 @@ const UserResearch = () => {
         </div>
 
         <div>
-          <p className="py-10 gap-4"><b>Who are we?</b><br />Marmalade AI was started by people who are all remote, fascinated by networking, have done a ton of freelancing, focus groups, and software products of all kinds, and are passionate about making this useful! We know we can't do it without your help!</p>
+          <p className="py-10 gap-4"><b>Who are we?</b><br />Marmalade AI was started by people who are all remote, fascinated by networking, and have done a ton of freelancing, focus groups, and all kinds of software, and are passionate about making this useful! We know we can't do it without your help!</p>
         </div>
         <div>
           <StaticImage alt="Who are we?" src="../../images/Screenshot at 2023-04-14 19-16-47_834.png"/>
