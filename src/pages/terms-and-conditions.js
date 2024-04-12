9655d061a060b786a7364d82e279e4185199dfd1
craigfisk <craigfisk@users.noreply.github.com>
2024-04-11 23:17:21 -0400

    Further edits to "terms-and-conditions.js".

--- src/pages/terms-and-conditions.js
+++ src/pages/terms-and-conditions.js
@@ -43,17 +43,17 @@ const TermsAndConditions = () => {
 <h2>Paid Accounts</h2>
 <br/>
 <p><i>Billing</i>: You can upgrade your account to access paid features, which would turn your 
-    account into a paid account. When you do that, we’ll automatically bill you from the date 
+    account into a paid account. When you do that, we'll automatically bill you from the date 
     you convert to a paid account and on each periodic renewal until cancellation. 
-    You are responsible for all applicable taxes, and we’ll charge tax when required. 
+    You are responsible for all applicable taxes, and we'll charge tax when required. 
     Some countries have mandatory local laws regarding your cancellation rights, and 
-    this paragraph doesn’t override these laws.</p>
+    this paragraph doesn't override these laws.</p>
     <br/>
 <p><i>Refunds</i>: You may cancel your Marmalade AI paid account at any time (instructions on how 
-to do that are [<b>TBD: </b>here]). Refunds are issued if they are required by law.</p>
+to do that are [TBD] <b>here</b>). Refunds are issued if they are required by law.</p>
 <br/>
-<p><i>Downgrades</i>: Your paid account will remain in effect until it’s canceled or terminated 
-under these terms. If you don’t pay for your paid account on time, we reserve the right to 
+<p><i>Downgrades</i>: Your paid account will remain in effect until it's canceled or terminated 
+under these terms. If you don't pay for your paid account on time, we reserve the right to 
 suspend it or reduce your access to the free version of our products.</p>
 <br/>
 <p><i>Free trials</i>: Marmalade AI may offer a free trial membership. If you are using a free trial 
@@ -69,7 +69,7 @@ those changes take effect. Any change in prices will apply to your next billing 
 <p><b>User Content</b> refers to the text and documents you enter, upload, and transmit when 
 you use our products.</p>
 <br/>
-<p>You own your User Content; Marmalade AI doesn’t own it. To provide our products to you, we need your 
+<p>You own your User Content; Marmalade AI doesn't own it. To provide our products to you, we need your 
 permission (in legal language, a license) to use your User Content.</p>
 <br/>
 <p>You grant us a license to your User Content for the limited purposes of:</p>
@@ -94,7 +94,7 @@ or license your User Content to third-party data brokers.</p>
 <li>&#x2713; Worldwide (so you can access your User Content from anywhere in the world);</li>
 <li>&#x2713; Non-exclusive (meaning you own your User Content and can also license it to other people 
     or companies);</li>
-<li>&#x2713; Royalty-free (meaning we don’t pay you for it); and</li>
+<li>&#x2713; Royalty-free (meaning we don't pay you for it); and</li>
 <li>&#x2713; For as long as intellectual property laws protect your User Content.</li>
 </ul>
 </p>
@@ -123,19 +123,19 @@ o you.</p>
 <br/>
 <h2>Your Use of Generative AI</h2>
 <br/>
-<p>Additional terms apply when you use Marmalade AI’s generative AI services and features.</p>
+<p>Additional terms apply when you use Marmalade AI's generative AI services and features.</p>
 <br/>
 <p>
 <i>Generated Content</i>. You may use generative AI features to submit inputs and receive generated outputs. 
 When you use generative AI features, the inputs and outputs are your User Data. You are responsible 
-for your User Data and will not use Marmalade AI’s generative AI features in a way that infringes, 
+for your User Data and will not use Marmalade AI's generative AI features in a way that infringes, 
 violates, or misappropriates any of our rights or the rights of any third party. You acknowledge 
 that due to the nature of machine learning and the technology powering generative AI, outputs may 
 not be unique and Marmalade AI may generate the same or similar output for third parties.</p>
 <br/>
 <p>
 <i>Usage Limits</i>. Depending on your subscription plan, you may have a limit on your use of 
-Marmalade AI’s generative AI features. You acknowledge and agree that if you exceed the usage limit 
+Marmalade AI's generative AI features. You acknowledge and agree that if you exceed the usage limit 
 allotted by your subscription plan: (i) you may be required to purchase additional usage to continue 
 accessing and using generative AI assistance and (ii) Marmalade AI may suspend or degrade performance 
 of the generative AI features. You acknowledge that any purchased usage for generative AI assistance 
@@ -143,45 +143,30 @@ is tied to your Marmalade AI subscription plan, and account termination will inv
 purchased usage.</p>
 <br/>
 <p>
-<i>Usage Restrictions</i>. You may not use Marmalade AI’s generative AI features:
+<i>Usage Restrictions</i>. You may not use Marmalade AI's generative AI features:
 <br/>
 <ul>
 <li>&#x2713; To develop foundation or large language models that compete with Marmalade AI;</li> 
-<li>&#x2713; To mislead any third party that any output from Marmalade AI’s generative AI was solely human generated;</li>
+<li>&#x2713; To mislead any third party that any output from Marmalade AI's generative AI was solely human generated;</li>
 <li>&#x2713; In a manner that violates these terms, Marmalade AI documentation, usage guidelines, or our Acceptable 
 Use Policy.</li>
 </ul>
 </p>
 <br/>
-{/*
-<h2>Marmalade AI Business and Marmalade AI for Education Users</h2>
-
-<p><i>Business terms</i>. When you sign up for Marmalade AI Business or Marmalade AI for Education, 
-your organization’s terms will govern your use of your Marmalade AI team account.</p>
-
-<p><i>Using Marmalade AI Business or Marmalade AI for Education.</i> If you join a Marmalade AI 
-Business or Marmalade AI for Education team account, you must use the products in compliance with 
-your organization’s terms and policies. Please note that Marmalade AI team accounts are subject to 
-your organization’s control. Your administrators can downgrade, restrict, or terminate your access 
-to a Marmalade AI team account and view information about it. Also, if you are a Marmalade AI for 
-Education team member, your parents and school administrator may have the legal right to access your 
-account or delete it.
-</p> 
-*/}
 
 <h2>Limitations on Use and Responsibilities</h2>
 <br/>
 <p><i>Acceptable Use Policy</i>. You must comply with our Acceptable Use Policy when you use our products.</p> 
 <br/>
 <p><i>Keeping your account information up to date and safe</i>. You are responsible for safeguarding your 
-password to our products. Don’t share your account credentials or give others access to your account. 
+password to our products. Don't share your account credentials or give others access to your account. 
 We will use your account email address to contact you about our products, so you must ensure that your 
 account information stays current.</p>
 <br/>
 <p><i>Minimum age requirements</i>. To use our products, you must be at least 13 if you reside in the United 
 States and 16 if you reside anywhere else. If the law where you reside requires that you are older 
 for us to lawfully provide our products to you without parental consent (including using your 
-information), you must be that age. You may not use our products if you don’t meet these age 
+information), you must be that age. You may not use our products if you don't meet these age 
 requirements. However, if your parents or guardians have allowed you to join a Marmalade AI for 
 Education team, you may still use our products.</p> 
 <br/>
@@ -193,17 +178,17 @@ Education team, you may still use our products.</p>
 <p>(a) you have breached these terms or our Acceptable Use Policy,</p>
 <p>(b) you use the products in a manner that would cause a real risk of harm or loss to other 
 Marmalade AI users or us, or</p>
-<p>(c) you don’t have a paid account and haven’t accessed our products for 12 consecutive months.</p>
+<p>(c) you don't have a paid account and haven't accessed our products for 12 consecutive months.</p>
 <br/>
-<p>Before suspending or terminating your account, we’ll (1) provide you with reasonable advance 
+<p>Before suspending or terminating your account, we'll (1) provide you with reasonable advance 
     notice via the email address associated with your account so you can try to remedy the activity 
     that prompted us to contact you and (2) allow you to export your documents from our products. If 
-    you fail to take the steps we ask of you after such notice, we’ll terminate or suspend your access 
+    you fail to take the steps we ask of you after such notice, we'll terminate or suspend your access 
     to our products.</p>
     <br/>
-<p>We won’t provide notice before termination where we believe that:</p>
+<p>We won't provide notice before termination where we believe that:</p>
 <br/>
-<p>(a) you’re in material breach of these terms or our Acceptable Use Policy,</p>
+<p>(a) you're in material breach of these terms or our Acceptable Use Policy,</p>
 <p>(b) doing so would cause us legal liability or compromise our ability to provide our products to our 
 other users, or</p>
 <p>(c) the law prohibits us from providing notice.</p>
@@ -211,7 +196,7 @@ other users, or</p>
 <p>When this agreement terminates for any reason, all the sections one would expect to survive will 
     survive, including, but not limited to, “Warranty Disclaimers,” “Limitation of Liability,” 
     Resolving Disputes,” “Miscellaneous Legal Terms,” “Our Intellectual Property Rights and License,” 
-    and “Paid Accounts.” The termination of this agreement doesn’t affect any amounts owed before that 
+    and “Paid Accounts.” The termination of this agreement doesn't affect any amounts owed before that 
     termination. </p>
     <br/>
 <h2>Discontinuation and Modification of Products</h2>
@@ -228,12 +213,12 @@ other users, or</p>
     <br/>
 <p>Unless the following restrictions are prohibited by law, you agree not to reverse engineer or 
 decompile our products, attempt to do so, or assist anyone in doing so. Our products may update 
-themselves automatically to ensure you’re using the latest version.</p>
+themselves automatically to ensure you're using the latest version.</p>
 <br/>
 <h2>Third-Party Websites</h2>
 <br/>
 <p>Any links to third-party websites or apps are provided for your convenience only and are subject to 
-the third party’s terms. Marmalade AI isn’t responsible or liable for those websites, products, or 
+the third party's terms. Marmalade AI isn't responsible or liable for those websites, products, or 
 services.</p>
 <br/>
 <h2>Beta Products</h2>
@@ -256,13 +241,13 @@ will log and analyze information about how you use and interact with them.</p>
     ARE PROVIDED ON AN “AS IS,” “WHERE IS,” “AS AVAILABLE,” “WITH ALL FAULTS” BASIS, WITHOUT WARRANTIES 
     OF ANY KIND, EITHER EXPRESS OR IMPLIED. WITHOUT LIMITING THE FOREGOING, THE Marmalade AI ENTITIES 
     DO NOT WARRANT THAT THE OPERATION OF OUR PRODUCTS WILL MEET YOUR REQUIREMENTS OR WILL BE UNINTERRUPTED 
-    OR ERROR-FREE. Marmalade AI’S AFFILIATES AND THE Marmalade AI ENTITIES’ SUPPLIERS, LICENSORS, AND 
+    OR ERROR-FREE. Marmalade AI'S AFFILIATES AND THE Marmalade AI ENTITIES' SUPPLIERS, LICENSORS, AND 
     DISTRIBUTORS ARE INTENDED THIRD-PARTY BENEFICIARIES OF THIS SECTION.</p>
     <br/>
 <p>THE Marmalade AI ENTITIES DO NOT MAKE ANY WARRANTY REGARDING THE OUTPUTS THAT MAY BE GENERATED FROM USE 
 OF GENERATIVE AI FEATURES, INCLUDING WITH RESPECT TO THE FACTUAL ACCURACY OF ANY OUTPUTS OR SUITABILITY 
 FOR YOUR USE CASE. YOU UNDERSTAND AND AGREE THAT YOUR USE OF ANY GENERATIVE AI OUTPUTS IS DONE AT YOUR 
-SOLE RISK. DUE TO THE NATURE OF GENERATIVE TECHNOLOGY, YOU SHOULD NOT RELY ON Marmalade AI’S 
+SOLE RISK. DUE TO THE NATURE OF GENERATIVE TECHNOLOGY, YOU SHOULD NOT RELY ON Marmalade AI'S 
 GENERATIVE AI AS A SINGLE SOURCE OF FACTUAL INFORMATION. NO INFORMATION OR ADVICE, WHETHER ORAL OR 
 WRITTEN, OBTAINED BY YOU FROM OR THROUGH Marmalade AI SHALL CREATE ANY WARRANTY NOT EXPRESSLY MADE 
 HEREIN.</p>
@@ -281,25 +266,25 @@ HEREIN.</p>
 FUNDAMENTAL ELEMENT OF THE BASIS OF THE BARGAIN BETWEEN Marmalade AI AND YOU. YOU UNDERSTAND THAT 
 OUR PRODUCTS WOULD NOT BE PROVIDED WITHOUT SUCH LIMITATIONS.</p>
 <br/>
-<p>Certain countries and states don’t allow the limitation of certain damages, so some or all of this 
+<p>Certain countries and states don't allow the limitation of certain damages, so some or all of this 
     limitation of liability may not apply to you, and you may have additional rights. In that case, 
-    you agree that the Marmalade AI Entities’ liability is limited to the maximum extent permissible 
+    you agree that the Marmalade AI Entities' liability is limited to the maximum extent permissible 
     in your country of residence.</p>
     <br/>
 <p>IF ANY PORTION OF THESE SECTIONS IS HELD TO BE INVALID, THE INVALIDITY OF SUCH PORTION SHALL NOT 
-AFFECT THE VALIDITY OF THE REMAINING PORTIONS OF THESE TERMS. Marmalade AI’S AFFILIATES AND THE 
-Marmalade AI ENTITIES’ SUPPLIERS, LICENSORS, AND DISTRIBUTORS ARE INTENDED THIRD-PARTY BENEFICIARIES OF 
+AFFECT THE VALIDITY OF THE REMAINING PORTIONS OF THESE TERMS. Marmalade AI'S AFFILIATES AND THE 
+Marmalade AI ENTITIES' SUPPLIERS, LICENSORS, AND DISTRIBUTORS ARE INTENDED THIRD-PARTY BENEFICIARIES OF 
 HIS SECTION.</p>
 <br/>
 <h2>Resolving Disputes</h2>
 <br/>
 <p>Before filing a claim against Marmalade AI, you agree to try 
     to resolve the dispute informally by contacting <a href="mailto:support@marmalade.ai">support@marmalade.ai</a>. 
-    We’ll try to resolve 
+    We'll try to resolve 
     the dispute informally by contacting you via email. If you and we cannot resolve the dispute 
     informally, then a party seeking to bring a formal proceeding must first send to the other, by 
     certified mail, a written Notice of Dispute (“Notice”). The Notice to Marmalade AI should be sent 
-    to [<b>TBD</b>] 10 Dobbs St, Bernardsville, NJ 07924, Attn: Legal Department. The Notice must 
+    to 10 Dobbs St, Bernardsville, NJ 07924, Attn: Legal Department. The Notice must 
     (i) describe the nature and basis of the claim or dispute and (ii) set forth the specific relief 
     sought. If Marmalade AI and you do not resolve the claim within sixty (60) calendar days after 
     receiving the Notice, you or Marmalade AI may commence a formal proceeding. </p>
@@ -309,9 +294,9 @@ relating to these terms for our products will be brought in the federal or state
 County, New Jersey, subject to the mandatory arbitration provisions below. Both you and Marmalade AI 
 consent to venue and personal jurisdiction in such courts. If you reside in a country with laws that 
 give consumers the right to bring disputes in their local courts (for example, European Union member 
-states), this paragraph doesn’t affect those requirements.</p>
+states), this paragraph doesn't affect those requirements.</p>
 <br/>
-<h3>IF YOU’RE A U.S. RESIDENT, YOU ALSO AGREE TO THE FOLLOWING MANDATORY ARBITRATION PROVISIONS:</h3>
+<h3>IF YOU'RE A U.S. RESIDENT, YOU ALSO AGREE TO THE FOLLOWING MANDATORY ARBITRATION PROVISIONS:</h3>
 <br/>
 <p><i>We both agree to arbitrate</i>. You and Marmalade AI agree to resolve any claims relating to these terms 
 or our products through final and binding arbitration by a single arbitrator, except as set forth 
@@ -334,7 +319,7 @@ is entitled.</p>
 <br/>
 <p><i>Arbitration fees and incentives</i>. The AAA rules will govern the payment of all arbitration fees. 
 Marmalade AI will pay all arbitration fees for individual arbitration for non-frivolous claims less 
-than $75,000. Marmalade AI will not seek its attorneys’ fees and costs in arbitration unless the 
+than $75,000. Marmalade AI will not seek its attorneys' fees and costs in arbitration unless the 
 arbitrator determines that your claim is frivolous.</p>
 <br/>
 <p><i>Exceptions to agreement to arbitrate</i>. Either you or Marmalade AI may assert claims, if they qualify, in 
@@ -351,7 +336,7 @@ to resolve your claim.</p>
 <p>You may only resolve disputes with us individually. That is, you may not 
 bring a claim as a plaintiff or a class member in a class, consolidated, or representative action. 
 Class arbitrations, class actions, private attorney general actions, and consolidation with other 
-arbitrations aren’t allowed.</p>
+arbitrations aren't allowed.</p>
 <br/>
 <p><i>Severability</i>. If the “NO CLASS ACTIONS” paragraph is held unenforceable, then the entirety of this 
 “Mandatory Arbitration Provisions” section will be deemed void. If you are found to have a non-waivable 
@@ -365,7 +350,7 @@ claims or requests for relief in arbitration.</p>
 <br/>
 <p>New Jersey law will govern these terms except for its conflicts of laws principles. However, some 
 countries (including those in the European Union) have laws that require agreements to be governed by 
-the local laws of the consumer’s country. This paragraph doesn’t override those laws.</p>
+the local laws of the consumer's country. This paragraph doesn't override those laws.</p>
 <br/>
 <h2>Miscellaneous Legal Terms</h2>
 <br/>
@@ -376,7 +361,7 @@ our products. They supersede any prior agreements.</p>
 <br/>
 <h3>Waiver, Severability, and Assignment</h3>
 <br/>
-<p>Marmalade AI’s failure to enforce a provision is not a waiver of its right to do so later. If a 
+<p>Marmalade AI's failure to enforce a provision is not a waiver of its right to do so later. If a 
 provision (other than the “NO CLASS ACTIONS” paragraph) is unenforceable, the remaining provisions 
 will remain in full effect, and an enforceable term will be substituted with the goal of reflecting 
 our intent as closely as possible. You may not assign any of your rights or obligations under these 
@@ -389,16 +374,16 @@ or subsidiaries or any successor in interest of any business associated with the
 regulations, and changes to our products.</p>
 <br/>
 <p>If an update significantly negatively affects your use of our products or your legal rights as a user 
-of our products, we’ll notify you before the update’s effective date by sending an email to the email 
+of our products, we'll notify you before the update's effective date by sending an email to the email 
 address associated with your account or via an in-product notification. We will give you at least 30 
 days after that notice before the update takes effect. </p>
 <br/>
-<p>We may also update these terms in ways that won’t significantly negatively affect your rights. For 
+<p>We may also update these terms in ways that won't significantly negatively affect your rights. For 
 example, we may change the feature or product names or change the email address you can use to 
 contact us. In those cases, we will post the change to our website and link to the previous version. </p>
 <br/>
-<p>If you don’t agree to the updates we make, you must cancel your account before they become effective. 
-Where required, we’ll offer you a prorated refund based on the amounts you have prepaid for our 
+<p>If you don't agree to the updates we make, you must cancel your account before they become effective. 
+Where required, we'll offer you a prorated refund based on the amounts you have prepaid for our 
 products and your account cancellation date. By using or accessing the products after the updates 
 come into effect, you agree to be bound by the revised terms.</p>
 <br/>
