9099b0a6ffaa4a995e56d3d3675959e008ddeb13
craigfisk <craigfisk@users.noreply.github.com>
2024-04-11 21:05:02 -0400

    Edited FAQ.

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -14,7 +14,7 @@ export const Head = () => (
       In 2023 we are working closely with specialists in UX design,
       digital marketing, and machine learning to understand networking problems and make this
       really work for people. The
-      Paul Graham &quot;Y Combinator&quot; mantra is &quot;do things that don't scale.&quot;"
+      Paul Graham 'Y Combinator' mantra is 'do things that don't scale.'"
     />
   </>
 );
@@ -30,14 +30,13 @@ const Faq = () => (
               <h2>Getting Started</h2>
 
               <h3>How do I start doing professional networking <i>virtually</i>?</h3>
-              <p>We haven&apos;t launched yet. We&quot;d love to listen to your needs and show you what we&apos;re building. 
-                Please click the &quot;book a demo&quot; button on the homepage.</p>
+              <p>We haven't launched yet. We'd love to listen to your needs and show you what we're building.</p>
 
               <h3>What does it cost?</h3>
-              <p>Free for Early Access users.</p>
+              <p>Free for beta users.</p>
               
               <h3>Is this a global service?</h3>
-              <p>No. The Early Access program is only available in the U.S.</p>
+              <p>No. The beta is only available in the U.S.</p>
 
             </div>
             <div className="pt-5">
@@ -45,23 +44,29 @@ const Faq = () => (
               <h2>Building Your Network</h2>
 
               <h3>How do I connect with other people?</h3>
-              <p>Fill out your profile, and you get a professional network that is virtual with other people that have registered. Check back regularly because the network will grow. You control communications.</p>
+              <p>Fill out your profile, and you get a professional network that is virtual with other people that have registered. Check back regularly because the network will grow.</p>
               
               <h3>Can I find a job through this?</h3>
-              <p>Well, 80% of jobs are found by networking, so sure, because being on the platform helps build your professional network.</p>
+              <p>80% of jobs are found by networking, so sure, being on the platform helps build 
+                your professional network. We're actually hearing from hiring managers that they now get
+                deluged by resumes tailored to match a job description and get 
+                past the ATS. All the more reason to network!</p>
 
               <h3>What can I learn through this?</h3>
-              <p>Your network is a great resource for learning and sharing whatever is new or relevant in your work.</p>
+              <p>Your virtual network is a great resource for learning and sharing whatever is new or relevant in your work.</p>
               
               <h3>What if I want to be mentored? Or to mentor?</h3>
               <p>Indicate that in your profile.</p>
 
               <h3>How do I contact people?</h3>
-              <p>For now, set up a Zoom call. We plan to incorporate video chat, including calls and scheduled calls, indicating you are or are not available, and so forth.</p>
+              <p>For now, set up a Zoom call. We plan to incorporate video chat, including calls and 
+                scheduled calls, indicating you are not available (default) or are available (when), 
+                about what topics, and so forth.</p>
               
               <h3>What if my specialization is not supported?</h3>
-              <p>Networking applies to everyone. For &quot;Early Access&quot;, the focus is on specific areas. If your specialization is outside of those areas,
-                you are welcome to participate, but you may not find the matching to be as good. We expect to expand over time.</p>
+              <p>Networking applies to everyone. In beta, the focus is on 
+                a small number of specific areas. If your specialization is something else,
+                you are welcome to participate, but you may not find the matching to be as good.</p>
             
             </div>
             <div className="pt-5">
@@ -88,7 +93,7 @@ const Faq = () => (
           <h2>Questions?</h2>
 
           <h3>Where else can I ask questions about Marmalade AI?</h3>
-          <p>Please <a href="mailto:info@marmalade.ai">contact us</a> or schedule a demo call:
+          <p>Please <a href="mailto:info@marmalade.ai">contact us</a>.
           </p>
           <div className="container flex flex-col items-center">
             <button className="hover:bg-orange-300 bg-orange-400 border-slate-800 mt-2 mb-2 px-4 py-0 rounded items-centerfont-bold" onClick={()=>{navigate("https://calendly.com/wcraigfisk/call-with-craig-fisk-30-minutes")}}>Schedule networking input</button>
