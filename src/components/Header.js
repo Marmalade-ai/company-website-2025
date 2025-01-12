1c0c71257c1bd5b05c8e8cfd343cff1b96e37a1a
Craig Fisk <craigfisk@marmalade.ai>
2025-01-12 18:10:12 -0500

    Effortless

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => (
   <header>
     <nav>
       <div className="text-xl">
-        <span class="text-[#ff7f00] font-bold">Marmalade AI</span> - <b>effortless networks!</b>
+        <span class="text-[#ff7f00] font-bold">Marmalade AI</span> - <b>effortless!</b>
 
         <button 
                   className="w-30 bg-[#ff7f00] bg-opacity-100 border border-width-1 hover:underline px-1 py-0 rounded-lg text-black float-right" 
