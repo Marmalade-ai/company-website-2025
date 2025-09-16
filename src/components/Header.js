e0735c3e04a678b6291b1b8e701a8641276522c2
Craig Fisk <craigfisk@marmalade.ai>
2025-09-16 19:38:35 -0400

    Various edits to Gatsby5 2025 site.

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,12 +7,12 @@ const Header = () => (
   <header>
     <nav>
       <div className="text-xl">
-        <span class="text-[#ff7f00] font-bold">Marmalade AI</span> - <b>effortless!</b>
+        <span class="text-[#ff7f00] font-bold">Marmalade AI</span> - <b>peer networking</b>
 
         <button 
                   className="w-30 bg-[#ff7f00] bg-opacity-100 border border-width-1 hover:underline px-1 py-0 rounded-lg text-black float-right" 
                   onClick={()=>{navigate("https://docs.google.com/forms/d/1eu_w17ssDesxl9iYB2DYgmg_sldKc3q_qBu0EuSAz1g/")}}>
-                    Free beta
+                    beta
         </button>
 
       </div>
