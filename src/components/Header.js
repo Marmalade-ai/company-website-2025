a832243fa9a780b2951d3c5804571e656df9ed11
Craig Fisk <craigfisk@marmalade.ai>
2025-09-29 20:17:08 -0400

    Changed color for beta to light orange.

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ const Header = () => (
         <span class="text-[#ff7f00] font-bold">Marmalade AI</span> - <b>peer networking</b>
 
         <button 
-                  className="w-30 bg-[#ff7f00] bg-opacity-100 border border-width-1 hover:underline px-1 py-0 rounded-lg text-black float-right" 
+                  className="w-40 bg-[#ff7f00] bg-opacity-20 border border-width-1 hover:underline px-1 py-0 text-black float-right" 
                   onClick={()=>{navigate("https://docs.google.com/forms/d/1eu_w17ssDesxl9iYB2DYgmg_sldKc3q_qBu0EuSAz1g/")}}>
                     beta
         </button>
